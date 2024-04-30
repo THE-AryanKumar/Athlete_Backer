5702a160be5e4df675d8572f95131e21a1b956bb
THE-AryanKumar <lit2021030@iiitl.ac.in>
2024-05-01 02:07:04 +0530

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Footer from "./(Footer)/Footer";
 import { Signup_data } from "../Redux-store/Redux-action";
 import { useSelector } from "react-redux";
 import Aadi from "./(Aadi_Special)/aadi";
+import Script from "next/script";
 
 export default function Home() {
   const Signup = useSelector(Signup_data);
@@ -15,6 +16,8 @@ export default function Home() {
     >
       <Home_Body />
       <Footer />
+      <Script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></Script>
+<Script src="https://mediafiles.botpress.cloud/6c0b34ce-2bb0-46c8-b54c-b010c7f76dac/webchat/config.js" defer></Script>
     </main>
   );
 }
