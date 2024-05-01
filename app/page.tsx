524b74bd8f112d60fb94703b05b94a4064341be8
THE-AryanKumar <lit2021030@iiitl.ac.in>
2024-05-01 15:08:00 +0530

    z-index fixed

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,11 @@ export default function Home() {
     >
       <Home_Body />
       <Footer />
+      <div className="z-20">
+
       <Script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></Script>
-<Script src="https://mediafiles.botpress.cloud/6c0b34ce-2bb0-46c8-b54c-b010c7f76dac/webchat/config.js" defer></Script>
+      <Script src="https://mediafiles.botpress.cloud/6c0b34ce-2bb0-46c8-b54c-b010c7f76dac/webchat/config.js" defer></Script>
+      </div>
     </main>
   );
 }
